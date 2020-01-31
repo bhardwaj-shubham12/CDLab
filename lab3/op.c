cd673a2c14c06918425e67a3e4b83d18f1f91c09
geekyaryan <50978992+geekyaryan@users.noreply.github.com>
2020-02-01 01:05:48 +0530

    Add files via upload

---
+++ lab3/op.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int fact(int n);
+
+int main(int argc)
+{
+	//comm
+	printf("Hello there");
+	int a=51;
+	char buf[]="Hello there";
+	//sprintf(buf,"%d",a);
+
+	printf("%s\n",buf);
+	
+	return 0;
+}
